78d089e7ff4eeb842f9e7d456ba93836b22028d6
DL-Berkey <ruyria@naver.com>
2022-10-31 00:55:10 +0900

    FE: use .env to port in vite.config.ts

---
+++ front/src/env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+    readonly VITE_PORT: number;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    const PORT = Number(env.VITE_PORT);
+
+    return {
+        plugins: [react()],
+        define: {
+            __APP__: env.APP_ENV,
+        },
+        server: {
+            port: PORT,
+        },
+    };
+});
