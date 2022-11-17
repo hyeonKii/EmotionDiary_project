a5acf0e8b608293b59834472b08bbf922582c5c9
DL-Berkey <ruyria@naver.com>
2022-11-18 04:16:58 +0900

    BE: FIX Merge에 의한 에러 수정

--- back/src/services/diaryService.ts
+++ back/src/services/diaryService.ts
@@ -12,7 +12,6 @@ class DiaryService {
                         nickname,
                     },
                 },
-                author,
                 title,
                 description,
             },
