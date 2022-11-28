0b5c1897e6f032b8e6f037fed8f8825c5b909a6d
이하늘 <wsxedc901@gmail.com>
2022-11-28 15:47:31 +0900

    FE: refactor: GlobalStyles에 button 스타일 추가 (초기화)

--- front/src/styles/GlobalStyle.ts
+++ front/src/styles/GlobalStyle.ts
@@ -13,4 +13,10 @@ export const GlobalStyle = createGlobalStyle`
       margin: 0;
       padding: 0;
     }
+
+    button {
+      cursor: pointer;
+      background-color: transparent;
+      border: none;
+    }
 `;
