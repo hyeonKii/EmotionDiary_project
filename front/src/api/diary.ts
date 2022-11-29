ab3e6846fc0ca216bd757afafe91511eff7f4368
이하늘 <wsxedc901@gmail.com>
2022-11-29 17:01:27 +0900

    FE: feat: Diary 관련 Api 추가 (생성, 조회, 삭제 요청 확인 완료)

--- front/src/api/diary.ts
+++ front/src/api/diary.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { URL } from "./url";
 import * as endpoint from "./constants/diaryEndpoints";
+import { useMutation, useQuery } from "react-query";
 
 interface WriteDiary {
     userID: string;
@@ -14,26 +15,71 @@ interface EditDiary {
     description: string;
 }
 
-export const writeDiary = (diaryData: WriteDiary) => {
-    return axios.post(URL + endpoint.DIARY_WRITE, diaryData);
+const writeDiary = (diaryData: WriteDiary) => {
+    return axios.post(URL + endpoint.DIARY_WRITE, diaryData, {
+        headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
+            Refreshtoken: sessionStorage.getItem("refreshToken"),
+        },
+    });
 };
 
-export const getDiary = (params: string) => {
-    return axios.get(URL + endpoint.DIARY_GET + params);
+const getDiary = (params: string) => {
+    return axios.get(URL + endpoint.DIARY_GET + params, {
+        headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
+            Refreshtoken: sessionStorage.getItem("refreshToken"),
+        },
+    });
 };
 
-export const getEmotionDiary = (params: string) => {
-    return axios.get(URL + endpoint.DIARY_GET + params);
+const getEmotionDiary = (params: string) => {
+    return axios.get(URL + endpoint.DIARY_GET + params, {
+        headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
+            Refreshtoken: sessionStorage.getItem("refreshToken"),
+        },
+    });
 };
 
-export const getMyDiary = (id: string) => {
-    return axios.get(URL + endpoint.MYDIARY + "/" + id);
+const getMyDiary = (id: string) => {
+    return axios.get(URL + endpoint.MYDIARY + "/" + id, {
+        headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
+            Refreshtoken: sessionStorage.getItem("refreshToken"),
+        },
+    });
 };
 
-export const editMyDiary = (myDiaryData: EditDiary, id: string) => {
-    return axios.put(URL + endpoint.MYDIARY + "/" + id, myDiaryData);
+const editMyDiary = (myDiaryData: EditDiary, id: string) => {
+    return axios.put(URL + endpoint.MYDIARY + "/" + id, myDiaryData, {
+        headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
+            Refreshtoken: sessionStorage.getItem("refreshToken"),
+        },
+    });
 };
 
-export const deleteMyDiary = (id: string) => {
-    return axios.delete(URL + endpoint.MYDIARY + "/" + id);
+const deleteMyDiary = (id: string) => {
+    return axios.delete(URL + endpoint.MYDIARY + "/" + id, {
+        headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`,
+            Refreshtoken: sessionStorage.getItem("refreshToken"),
+        },
+    });
 };
+
+export const useRequestWriteDiary = (diaryData, options?) =>
+    useMutation(() => writeDiary(diaryData), options);
+
+export const useRequestEditDiary = (diaryData, id, options?) =>
+    useMutation(() => editMyDiary(diaryData, id), options);
+
+export const useRequestDeleteDiary = (id, options?) =>
+    useMutation(() => deleteMyDiary(id), options);
+
+export const useRequestGetAllDiaries = (key, params, options?) =>
+    useQuery(key, () => getDiary(params), options);
+
+export const useRequestGetDiary = (key, id, options?) =>
+    useQuery(key, () => getMyDiary(id), options);
