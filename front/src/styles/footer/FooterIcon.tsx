ebbb00194e50e5e1e79c29002870175d59d59fe1
hyeonki <khk5676@gmail.com>
2022-12-15 13:07:10 +0900

    Fe: style: footer git 경로 설정, 헤더 너비 설정

--- front/src/styles/footer/FooterIcon.tsx
+++ front/src/styles/footer/FooterIcon.tsx
@@ -6,16 +6,18 @@ interface GitIconProps {
 
 export default function footerIcon({ icon }: GitIconProps) {
     return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            style={{
-                width: "4.5rem",
-                height: "4.5rem",
-                marginTop: "2rem",
-            }}
-            viewBox={gitIconSet[icon].viewBox}
-        >
-            <path d={gitIconSet[icon].path} fill={`${gitIconSet[icon].fill}`}/>
-        </svg>
+            <a href={`${gitIconSet[icon].href}`}>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    style={{
+                        width: "4.5rem",
+                        height: "4.5rem",
+                        marginTop: "2rem",
+                    }}
+                    viewBox={gitIconSet[icon].viewBox}
+                >
+                    <path d={gitIconSet[icon].path} fill={`${gitIconSet[icon].fill}`}/>
+                </svg>
+            </a>
     );
 };
