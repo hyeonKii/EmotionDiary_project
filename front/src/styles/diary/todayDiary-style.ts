48adf7fd480598816650d9a0b92b2d246b857fbe
이하늘 <wsxedc901@gmail.com>
2022-12-16 23:49:56 +0900

    FE: fix: 글씨 색깔 고침

--- front/src/styles/diary/todayDiary-style.ts
+++ front/src/styles/diary/todayDiary-style.ts
@@ -40,46 +40,82 @@ export const TodaySection = styled.section`
         .react-calendar__tile.normal {
             background: ${color.lightGray};
             border-radius: 50%;
+
+            abbr {
+                color: white;
+            }
         }
 
         .react-calendar__tile.슬픔 {
             background: ${color.lightBlue};
             border-radius: 50%;
+
+            abbr {
+                color: white;
+            }
         }
 
         .react-calendar__tile.분노 {
             background: ${color.red};
             border-radius: 50%;
+
+            abbr {
+                color: white;
+            }
         }
 
         .react-calendar__tile.신남 {
             background: ${color.yellow};
             border-radius: 50%;
+
+            abbr {
+                color: white;
+            }
         }
 
         .react-calendar__tile.자신감 {
             background: ${color.orange};
             border-radius: 50%;
+
+            abbr {
+                color: white;
+            }
         }
 
         .react-calendar__tile.만족감 {
             background: ${color.pink};
             border-radius: 50%;
+
+            abbr {
+                color: white;
+            }
         }
 
         .react-calendar__tile.편안함 {
             background: ${color.skyBlue};
             border-radius: 50%;
+
+            abbr {
+                color: white;
+            }
         }
 
         .react-calendar__tile.불안 {
             background: ${color.green};
             border-radius: 50%;
+
+            abbr {
+                color: white;
+            }
         }
 
         .react-calendar__tile.상처 {
             background: ${color.deepBlue};
             border-radius: 50%;
+
+            abbr {
+                color: white;
+            }
         }
 
         .react-calendar__tile--now {
