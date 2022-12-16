ebf5acda2234585a21c6d20b450e714d0308d804
jlee7003 <jlee7003@naver.com>
2022-12-16 14:27:30 +0900

    FIX

--- front/src/hooks/useSetUser.tsx
+++ front/src/hooks/useSetUser.tsx
@@ -4,7 +4,7 @@ import { useSetRecoilState } from "recoil";
 
 interface Response {
     data: {
-        User: { nickname: string };
+        User: { nickname: string; id: number };
         certified_account: boolean;
     };
 }
@@ -22,13 +22,13 @@ export default function useSetUser() {
 
         onSuccess: (res: Response) => {
             const {
-                User: { nickname },
+                User: { nickname, id },
                 certified_account,
             } = res.data;
 
             console.log("로그인 성공");
 
-            setUserState({ nickname });
+            setUserState({ nickname, id });
         },
 
         onError: (error: Error) => {
