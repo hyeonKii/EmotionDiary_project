0c7849345efc34d26ec25f443f084ec012cd474c
jlee7003 <jlee7003@naver.com>
2022-12-16 14:48:17 +0900

    포트수정

--- front/src/components/chat/Chat.tsx
+++ front/src/components/chat/Chat.tsx
@@ -9,8 +9,8 @@ import { recentlyMsgState } from "@/temp/ChatRecoil";
 import { useRecoilValue, useRecoilState } from "recoil";
 import ChatRoom from "@/components/chat/chatroom";
 import { currentroom } from "@/temp/ChatRecoil";
-export const socket = io("http://localhost:3002");
-// export const socket = io("http://kdt-ai5-team02.elicecoding.com");
+// export const socket = io("http://localhost:3002");
+export const socket = io("http://kdt-ai5-team02.elicecoding.com");
 
 //채팅 상자
 interface ChatData {
