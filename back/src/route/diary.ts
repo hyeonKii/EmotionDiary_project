ea374a0335a3a33cba7693dc3ae0ed6a29ff51fc
jlee7003 <jlee7003@naver.com>
2022-12-17 01:35:48 +0900

    console

--- back/src/route/diary.ts
+++ back/src/route/diary.ts
@@ -115,6 +115,7 @@ diaryRouter.get(
         const year = now.getFullYear();
         const month = now.getMonth() + 1;
         const date = now.getDate();
+        console.log(year, month, date);
 
         const day = `${year}-${month >= 10 ? month : "0" + month}-${
             date >= 10 ? date : "0" + date
